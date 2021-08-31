d8e89e76d7d9446f032ebb9187b3d1bb547d2add
gustavo torregrosa <gustavo.torregrosa@gmail.com>
2021-08-31 09:07:52 -0300

    parcial

--- desafio_1/src/app.ts
+++ desafio_1/src/app.ts
@@ -1,8 +1,34 @@
 import express, { Application } from 'express'
 import routes from './routes'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+const swaggerOptions: swaggerJsDoc.Options = {
+    swaggerDefinition: {
+        info: {
+            title: 'Minha API',
+            description: 'API teste documentacao',
+            contact: {
+                name: 'Gustavo Torregrosa'
+            },
+            servers: ['http://localhost:5000'],
+            version: '2'
+        }
+    },
+    apis: ['app.js'],
+    
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
 const app: Application = express()
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(express.json())
+
+/**
+ * 
+ * @swagger
+ */
 app.use(routes)
 
 app.listen(5000, () => console.log('server on'))
--- desafio_1/src/routes/index.ts
+++ desafio_1/src/routes/index.ts
@@ -2,6 +2,10 @@ import { Router } from 'express'
 import pedidosRouter from './pedidos.routes'
 
 const routes = Router()
+/**
+ * 
+ * @swagger
+ */
 routes.use('/pedidos', pedidosRouter)
 
 export default routes
